9e7406568d4cff22d3828890166097a90f0da7f5
Jack Moon <rokmc756@gmail.com>
2021-11-12 16:02:57 +0900

    c

--- install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-ansible-playbook -u jomoon setup-kafka.yml -i ansible-hosts --tags="firewalld,java,kafka"
-# ansible-playbook -u jomoon kafka.yml -i hosts --tags="firewalld,java,kafka,zookeeper"
