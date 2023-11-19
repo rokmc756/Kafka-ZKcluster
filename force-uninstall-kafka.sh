c6e6b4c285fb906499247d415ddc20109bcdf60d
Jack Moon <rokmc756@gmail.com>
2023-11-19 18:37:29 +0900

    Update for modifying zookeeper interacted

--- force-uninstall-kafka.sh
+++ force-uninstall-kafka.sh
@@ -2,12 +2,16 @@
 
 # HOSTS="rk9-master rk9-slave rk9-node01 rk9-node02 rk9-node03"
 # HOSTS="rk8-master rk8-slave rk8-node01 rk8-node02 rk8-node03"
-# HOSTS="co7-master co7-slave co7-node01 co7-node02 co7-node03"
-HOSTS="rh8-master rh8-slave rh8-node01 rh8-node02 rh8-node03"
+HOSTS="co7-master co7-slave co7-node01 co7-node02 co7-node03"
+# HOSTS="rh8-master rh8-slave rh8-node01 rh8-node02 rh8-node03"
 for i in `echo "$HOSTS"`
 do
+    ssh root@$i "systemctl stop kafaka-ui"
     ssh root@$i "systemctl stop kafaka"
     ssh root@$i "systemctl stop zookeeper"
+    ssh root@$i "systemctl disable kafaka-ui"
+    ssh root@$i "systemctl disable kafaka"
+    ssh root@$i "systemctl disable zookeeper"
     ssh root@$i "killall java"
 done
 
