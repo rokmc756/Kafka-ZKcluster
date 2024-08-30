fd17a9dad29bd4cf2d48d42a8ca75c5932cdb391
Jack Moon <rokmc756@gmail.com>
2024-08-30 10:30:41 +0900

    Updated for Running Kafka and Kafka-UI after Reboot

--- roles/java/templates/java_home.sh
+++ roles/java/templates/java_home.sh
@@ -1,3 +1,3 @@
-# jdk_version=$(ls -al {{jvm_home}}|grep "^d"|grep "java"|awk '{print$NF}')
-# export JAVA_HOME={{ jvm_home }}/$jdk_version
+# jdk_version=$(ls -al {{ jdk.oss.jvm_home}}|grep "^d"|grep "java"|awk '{print$NF}')
+# export JAVA_HOME={{ jdk.oss.jvm_home }}/$jdk_version
 export JAVA_HOME=$(dirname $(dirname $(readlink $(readlink $(which java)))))
